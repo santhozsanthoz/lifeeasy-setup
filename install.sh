164d3d892fcf9c61156813bc3c192fd355219589
Santhosh Kumar <104722803+santhozsanthoz@users.noreply.github.com>
2024-11-05 18:06:09 +0530

    Updated the url of the repositories

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-git clone https://github.com/santhoshkumarr231299/lifeeasy-client.git
-git clone https://github.com/santhoshkumarr231299/lifeeasy-server.git
-git clone https://github.com/santhoshkumarr231299/lifeeasy-chat-server.git
+git clone https://github.com/santhozsanthoz/lifeeasy-client.git
+git clone https://github.com/santhozsanthoz/lifeeasy-server.git
+git clone https://github.com/santhozsanthoz/lifeeasy-chat-server.git
